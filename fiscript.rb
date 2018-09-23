e6933c28a1fe18e0dad4de103aa438440107022c
Mortennn <34214574+Mortennn@users.noreply.github.com>
2018-09-23 14:19:53 +0200

    Update fiscript.rb

--- fiscript.rb
+++ fiscript.rb
@@ -2,8 +2,7 @@ cask 'fiscript' do
   version '1.0.1'
   sha256 '66c1dfb84573e57e5e614fb679c81aabf89b286020d20f8a3d40d2af2b05f6a2'
 
-  url 'https://github.com/Mortennn/FiScript/releases/download/v1.0.1/FiScript.dmg'
-  appcast 'https://github.com/Mortennn/FiScript/releases/download/v1.0.1/appcast.xml'
+  url 'https://github.com/Mortennn/FiScript/releases/download/v1.0.1/FiScript.zip'
   name 'FiScript'
   homepage 'https://github.com/Mortennn/FiScript'
 
